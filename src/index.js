5b35ac4621c115f521a86114f67e469632026207
TraceLD <lukaszdabrowski881@gmail.com>
2021-06-08 21:11:15 +0200

    add banning

---
+++ src/index.js
@@ -0,0 +1,48 @@
+const Discord = require('discord.js');
+const config = require('./config.json');
+
+const client = new Discord.Client({
+  ws: {
+    intents: ['GUILD_MEMBERS', 'GUILD_MESSAGES', 'GUILDS']
+  }
+});
+
+client.once('ready', () => {
+  console.log('Bot is ready.');
+});
+
+client.login(config.token);
+
+client.on('guildMemberAdd', async (member) => {
+  console.log(member.displayName);
+
+  const patterns = config.patterns;
+
+  for (const pattern of patterns) {
+    if (
+      member.displayName.toLowerCase().includes(pattern) ||
+      member.nickname.toLowerCase().includes(pattern)
+    ) {
+      await member.ban();
+
+      const logsChannel = client.channels.cache.get(config.logsChannelId);
+
+      await logsChannel.send(
+        `Member ${member.displayName} (id: \`${member.id}\`) has been banned because their ID matched the pattern \`${pattern}\`.`
+      );
+    }
+  }
+});
+
+client.on('message', async (message) => {
+  if (message.content !== '.cleanup') {
+    return;
+  }
+
+  const members = await message.guild.members.fetch();
+  const guildMembers = members.values();
+
+  for (const gm of guildMembers) {
+    console.log(gm.id);
+  }
+});
